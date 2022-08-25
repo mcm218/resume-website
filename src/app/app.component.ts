40f7a52e4b7d852446e1997632a0012950be3a2e
Michael Muniz <mcmuniz218@gmail.com>
2022-08-25 15:27:09 -0400

    update header and deploy changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import JsonData from '../assets/me.json';
 import { ContactMe } from './models/contact-me';
 import { Education } from './models/education';
@@ -10,10 +10,22 @@ import { SkillBlock } from './models/skill-block';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Resume';
   experienceList: Array <Role> = JsonData.experience;
   skillsLists: Array <SkillBlock> = JsonData.skills;
   contact: ContactMe = JsonData.contact;
   education: Education = JsonData.education;
+
+  @ViewChild ('app_container') appContainerElement!: ElementRef;
+
+  ngAfterViewInit () {
+    // Are we on a mobile device?
+    if (window.screen.width < 600) {
+      let appContainerCSS = getComputedStyle (this.appContainerElement.nativeElement);
+      console.log(appContainerCSS.backgroundSize);
+      this.appContainerElement.nativeElement.style.backgroundSize = "auto " + window.screen.height + "px";
+      console.log(appContainerCSS.backgroundSize);
+    }
+  }
 }
