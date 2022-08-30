f87fd585ec30f5dd2b6fda72cd1a7e2b529902fb
Michael Muniz <mcmuniz218@gmail.com>
2022-08-30 08:24:42 -0400

    exp card tests

--- src/app/experience-card/experience-card.component.spec.ts
+++ src/app/experience-card/experience-card.component.spec.ts
@@ -23,66 +23,90 @@ describe('ExperienceCardComponent', () => {
     expect(component).toBeTruthy();
   });
   
-  xit('should have a company', () => {
-    component.item.company = "Test End Date";
+  it('should have a company', () => {
+    component.item.company = "Test Company";
     fixture.detectChanges();
 
     const componentElement: HTMLElement = fixture.nativeElement;
-    const sectionElement = componentElement.querySelector ('section')!;
-    expect(sectionElement.textContent).toContain (component.item.company);
+    const companyElement = componentElement.querySelector ('.company')!;
+    expect(companyElement.textContent).toContain (component.item.company);
   })
   
-  xit('shouldn\'t have a company', () => {
-    component.item.company = "Test End Date";
+  it('shouldn\'t have a company', () => {
+    component.item.company = "";
     fixture.detectChanges();
 
     const componentElement: HTMLElement = fixture.nativeElement;
-    const sectionElement = componentElement.querySelector ('section')!;
-    expect(sectionElement.textContent).toContain (component.item.company);
+    const companyElement = componentElement.querySelector ('.company')!;
+    expect(companyElement).toBeFalsy ();
   })
   
-  xit('should have a role', () => {
-    component.item.company = "Test End Date";
+  it('should have a role', () => {
+    component.item.role = "Test Role";
     fixture.detectChanges();
 
     const componentElement: HTMLElement = fixture.nativeElement;
     const sectionElement = componentElement.querySelector ('section')!;
-    expect(sectionElement.textContent).toContain (component.item.company);
+    expect(sectionElement.textContent).toContain (component.item.role);
   })
   
-  xit('should have a dates', () => {
-    component.item.company = "Test End Date";
+  it('should have a dates', () => {
+    component.item.startDate = "a";
+    component.item.endDate = "b";
     fixture.detectChanges();
 
     const componentElement: HTMLElement = fixture.nativeElement;
-    const sectionElement = componentElement.querySelector ('section')!;
-    expect(sectionElement.textContent).toContain (component.item.company);
+    const datesElement = componentElement.querySelector ('.dates')!;
+    expect(datesElement.textContent).toContain (`a - b`);
   })
   
-  xit('shouldn\'t have a dates', () => {
-    component.item.company = "Test End Date";
+  it('shouldn\'t have a dates', () => {
+    component.item.startDate = "";
     fixture.detectChanges();
 
     const componentElement: HTMLElement = fixture.nativeElement;
-    const sectionElement = componentElement.querySelector ('section')!;
-    expect(sectionElement.textContent).toContain (component.item.company);
+    const datesElement = componentElement.querySelector ('.dates')!;
+    expect(datesElement).toBeFalsy ();
   })
   
-  xit('should have a location', () => {
-    component.item.company = "Test End Date";
+  it('should have a location', () => {
+    component.item.location = "Test Location";
     fixture.detectChanges();
 
     const componentElement: HTMLElement = fixture.nativeElement;
-    const sectionElement = componentElement.querySelector ('section')!;
-    expect(sectionElement.textContent).toContain (component.item.company);
+    const locationElement = componentElement.querySelector ('.location')!;
+    expect(locationElement.textContent).toContain (component.item.location);
   })
   
-  xit('should have a list of notes', () => {
-    component.item.company = "Test End Date";
+  it('should have a list of notes', () => {
+    component.item.notes = new Array <string> ();
+    component.item.notes.push ("item 0");
+    component.item.notes.push ("item 1");
+    component.item.notes.push ("item 2");
+    component.item.notes.push ("item 3");
+
     fixture.detectChanges();
 
     const componentElement: HTMLElement = fixture.nativeElement;
-    const sectionElement = componentElement.querySelector ('section')!;
-    expect(sectionElement.textContent).toContain (component.item.company);
+    const ulElement = componentElement.querySelector <HTMLUListElement> ('ul')!;
+    expect(ulElement).toBeTruthy ();
+
+    let index = 0;
+    component.item.notes.forEach(note => {
+      expect (ulElement.children [index].textContent).toContain (note);
+      index++;
+    });
+  })
+
+  
+  it('should have no notes', () => {
+    component.item.notes = new Array <string> ();
+    
+    fixture.detectChanges();
+
+    const componentElement: HTMLElement = fixture.nativeElement;
+    const ulElement = componentElement.querySelector <HTMLUListElement> ('ul')!;
+    expect(ulElement).toBeTruthy ();
+    expect (ulElement.children.length).toEqual (0);
   })
 });
