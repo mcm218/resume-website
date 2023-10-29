ab8afc2a79a637e67e711754d493658477487a65
Michael Muniz <mcmuniz218@gmail.com>
2023-10-29 19:00:02 -0400

    updates

--- src/app/experience-list/experience-list.component.ts
+++ src/app/experience-list/experience-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ExperienceItem } from '../models/experience-item';
 import { ExperienceCardComponent } from '../experience-card/experience-card.component';
 import { Role } from '../models/role';
+import moment from 'moment';
 
 @Component({
-  selector: 'app-experience-list',
-  templateUrl: './experience-list.component.html',
-  styleUrls: ['./experience-list.component.scss']
+    selector: 'app-experience-list',
+    templateUrl: './experience-list.component.html',
+    styleUrls: ['./experience-list.component.scss'],
 })
-export class ExperienceListComponent {
-  @Input () role: Role = new Role ();
-  
-  constructor() { }
+export class ExperienceListComponent implements OnInit {
+    @Input() role: Role = new Role();
+
+    constructor() {}
+
+    ngOnInit(): void {
+        if (this.role.experience) {
+            this.role.experience.sort((a, b) =>
+                moment(a.startDate).isBefore(b.startDate) ? 1 : -1
+            );
+        }
+    }
 }
